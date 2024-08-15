81e02aec588072e53c254324480054518dc08101
Eivind Stormyr <eivind.stormyr@gmail.com>
2024-08-15 18:24:39 +0200

    Added new files and updated existing ones

--- src/app/shared/components/TagTable/TagTable.component.ts
+++ src/app/shared/components/TagTable/TagTable.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 interface Tag {
   [key: string]: {
     data: {
       value: {
-         value: string,
-         dataType: string
+        value: string,
+        dataType: string
       };
       statusCode: { value: number };
     };
@@ -17,31 +17,44 @@ interface Tag {
   templateUrl: './TagTable.component.html',
   styleUrls: ['./TagTable.component.css']
 })
-export class TagTableComponent implements OnInit {
+export class TagTableComponent implements OnInit, OnChanges {
   @Input() tags: Tag[] = [];
+  filteredTags: Tag[] = [];
+  filterString: string = '';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.filteredTags = this.tags;
+  }
+
+  // Detect changes in the @Input() tags
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['tags']) {
+      this.filterTags();  // Apply filter when tags input changes
+    }
+  }
 
   getStatusCodeDescription(value: number): string | undefined {
     const statusCategories: { [key: number]: string } = {
-      0x00000000: "Good",
-      0x40000000: "Uncertain",
-      0x80000000: "Bad"
+      0x00000000: 'Good',
+      0x40000000: 'Uncertain',
+      0x80000000: 'Bad'
     };
 
     return statusCategories[value];
   }
+
   getTagName(tag: Tag): string {
     return Object.keys(tag)[0];
   }
-  
+
   getTagValue(tag: Tag): string {
     const tagname = this.getTagName(tag);
-    return tag[tagname].data.value.value;
+    const value = tag[tagname]?.data?.value?.value;
+    return typeof value === 'string' ? value : String(value);
   }
-  
+
   getTagStatus(tag: Tag): number {
     const tagname = this.getTagName(tag);
     return tag[tagname].data.statusCode.value;
@@ -51,6 +64,7 @@ export class TagTableComponent implements OnInit {
     const tagname = this.getTagName(tag);
     return tag[tagname].data.value.dataType;
   }
+
   getStatusCodeIconClass(value: number): string {
     const statusCategories: { [key: number]: string } = {
       0x00000000: 'fas fa-check-circle fa-lg', // Green check for "Good"
@@ -60,5 +74,15 @@ export class TagTableComponent implements OnInit {
 
     return statusCategories[value] || 'fas fa-question-circle text-secondary'; // Default to a question mark if no match
   }
-  
+
+  filterTags(): void {
+    const lowerFilterString = this.filterString.toLowerCase();
+    this.filteredTags = this.tags.filter(tag => {
+      const tagName = this.getTagName(tag).toLowerCase();
+      // Filter only by TagName
+      return tagName.includes(lowerFilterString);
+    });
+  }
 }
+
+
